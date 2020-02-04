9cd05174a9c23c02c57c28e8bd5d526edefe26d4
Emma <emmagray183@gmail.com>
2020-02-04 17:33:49 +0000

    adds interfaces

--- src/test/java/attractions/RollercoasterTest.java
+++ src/test/java/attractions/RollercoasterTest.java
@@ -2,16 +2,26 @@ package attractions;
 
 import org.junit.Before;
 import org.junit.Test;
+import people.Visitor;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class RollercoasterTest {
 
     RollerCoaster rollerCoaster;
+    Visitor visitor1;
+    Visitor visitor2;
+    Visitor visitor3;
+    Visitor visitor4;
 
     @Before
     public void setUp() {
+
         rollerCoaster = new RollerCoaster("Blue Ridge", 10);
+        visitor1 = new Visitor(10, 164, 10);
+        visitor2 = new Visitor(13, 164, 10);
+        visitor3 = new Visitor(10, 140, 10);
+        visitor4 = new Visitor(13, 140, 10);
     }
 
     @Test
@@ -27,5 +37,13 @@ public class RollercoasterTest {
     @Test
     public void hasVisitCount() {
         assertEquals(0, rollerCoaster.getVisitCount());
+    }
+
+    @Test
+    public void hasHeightAndAgeRestrictions(){
+        assertTrue(rollerCoaster.isAllowedTo(visitor2));
+        assertFalse(rollerCoaster.isAllowedTo(visitor1));
+        assertFalse(rollerCoaster.isAllowedTo(visitor3));
+        assertFalse(rollerCoaster.isAllowedTo(visitor4));
     }
 }
--- src/test/java/stalls/TobaccoStallTest.java
+++ src/test/java/stalls/TobaccoStallTest.java
@@ -2,16 +2,22 @@ package stalls;
 
 import org.junit.Before;
 import org.junit.Test;
+import people.Visitor;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class TobaccoStallTest {
 
     TobaccoStall tobaccoStall;
+    Visitor visitor1;
+    Visitor visitor2;
+
 
     @Before
     public void setUp() throws Exception {
-        tobaccoStall = new TobaccoStall("Jacks Drum", "Jack Jarvis", ParkingSpot.B1);
+        tobaccoStall = new TobaccoStall("Jacks Drum", "Jack Jarvis", ParkingSpot.B1, 8);
+        visitor1 = new Visitor(14, 164, 10);
+        visitor2 = new Visitor(19, 164, 10);
     }
 
     @Test
@@ -27,5 +33,11 @@ public class TobaccoStallTest {
     @Test
     public void hasParkingSpot() {
         assertEquals(ParkingSpot.B1, tobaccoStall.getParkingSpot());
+    }
+
+    @Test
+    public void hasMinAge18(){
+        assertTrue(tobaccoStall.isAllowedTo(visitor2));
+        assertFalse(tobaccoStall.isAllowedTo(visitor1));
     }
 }
